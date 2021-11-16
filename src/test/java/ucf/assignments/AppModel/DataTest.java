0b57b14483771586f40ccdd4084c53060e078716
JazzFaye <jazzfaye@knights.ucf.edu>
2021-11-15 20:01:29 -0500

    Initial Commit

--- src/test/java/ucf/assignments/AppModel/DataTest.java
+++ src/test/java/ucf/assignments/AppModel/DataTest.java
@@ -6,4 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DataTest {
 
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getTodoItems() {
+    }
+
+    @Test
+    void addTodoItem() {
+    }
+
+    @Test
+    void updateTodoItem() {
+    }
+
+    @Test
+    void loadTodoItems() {
+    }
+
+    @Test
+    void saveTodoItems() {
+    }
 }
--- src/test/java/ucf/assignments/AppModel/TodoListTest.java
+++ src/test/java/ucf/assignments/AppModel/TodoListTest.java
@@ -2,13 +2,43 @@ package ucf.assignments.AppModel;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TodoListTest {
 
+    @Test
+    void getTitle() {
+    }
 
+    @Test
+    void setTitle() {
+    }
 
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getDue_Date() {
+    }
+
+    @Test
+    void setDue_Date() {
+    }
+
+    @Test
+    void getComplete() {
+    }
+
+    @Test
+    void setComplete() {
+    }
+
+    @Test
+    void testToString() {
+    }
 }
